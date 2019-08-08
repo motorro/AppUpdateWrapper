f21ad54ecb412e712ca92cd60a3c38196e66475c
Motorro <motorro@gmail.com>
2019-08-08 13:28:17 +0300

    Copyright

--- appupdatewrapper/src/main/java/com/motorro/appupdatewrapper/safeListeners.kt
+++ appupdatewrapper/src/main/java/com/motorro/appupdatewrapper/safeListeners.kt
@@ -1,8 +1,22 @@
+/*
+ * Copyright 2019 Nikolai Kotchetkov (motorro).
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+
 package com.motorro.appupdatewrapper
 
 import android.os.Handler
-import android.os.Looper
-import android.os.Looper.*
+import android.os.Looper.getMainLooper
 import androidx.annotation.VisibleForTesting
 import com.google.android.play.core.appupdate.AppUpdateManager
 import com.google.android.play.core.install.InstallStateUpdatedListener
--- appupdatewrapper/src/test/java/com/motorro/appupdatewrapper/SafeListenersKtTest.kt
+++ appupdatewrapper/src/test/java/com/motorro/appupdatewrapper/SafeListenersKtTest.kt
@@ -1,19 +1,33 @@
+/*
+ * Copyright 2019 Nikolai Kotchetkov (motorro).
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+
 package com.motorro.appupdatewrapper
 
 import android.app.Activity
-import android.os.Looper
-import android.os.Looper.*
+import android.os.Looper.getMainLooper
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.android.play.core.appupdate.testing.FakeAppUpdateManager
 import com.google.android.play.core.install.InstallState
 import com.google.android.play.core.install.InstallStateUpdatedListener
 import com.google.android.play.core.install.model.AppUpdateType
 import com.nhaarman.mockitokotlin2.mock
-import org.junit.Assert.*
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.Shadows.*
+import org.robolectric.Shadows.shadowOf
 import org.robolectric.annotation.LooperMode
 import kotlin.test.assertFailsWith
 
