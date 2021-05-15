a83a7ca7e7a849329f8f7850fa34b9f83572a0d7
motorro <motorro@gmail.com>
2021-05-15 08:00:32 +0300

    State logging references: #18

--- sample/src/main/java/com/motorro/appupdatewrapper/sample/MainActivity.kt
+++ sample/src/main/java/com/motorro/appupdatewrapper/sample/MainActivity.kt
@@ -2,7 +2,6 @@ package com.motorro.appupdatewrapper.sample
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
@@ -27,7 +26,7 @@ class MainActivity : AppCompatActivity(), AppUpdateView {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        // Starts flexible update flow that (if cancelled) will ask again tomorrow
+        // Starts flexible update flow
         updateWrapper = startFlexibleUpdate(
             AppUpdateManagerFactory.create(this.applicationContext),
             this,
@@ -44,6 +43,43 @@ class MainActivity : AppCompatActivity(), AppUpdateView {
     override val activity: Activity = this
 
     /**
+     * Called when update is checking or downloading data
+     * Subclasses may display some spinner at this point.
+     * Remove spinner in:
+     * - [updateReady]
+     * - [updateComplete]
+     * - [updateFailed]
+     */
+    override fun updateChecking() {
+        log("updateChecking")
+    }
+
+    /**
+     * Called when user accepts update download in play-core dialog and download starts.
+     * Called in flexible flow
+     */
+    override fun updateDownloadStarts() {
+        log("updateDownloadStarts")
+    }
+
+    /**
+     * Called when installation activity launches
+     * Calling this handler means the application will be restarted if update succeeds
+     * Subclass may want to finish it's current activity at this point
+     */
+    override fun updateInstallUiVisible() {
+        log("updateInstallUiVisible")
+    }
+
+    /**
+     * No update available or update flow completed
+     * Called in flexible flow
+     */
+    override fun updateComplete() {
+        log("updateComplete")
+    }
+
+    /**
      * Reports update is downloaded and ready to be installed
      * When ready to proceed call [AppUpdateState.userConfirmedUpdate]
      * Called in flexible flow
@@ -51,6 +87,7 @@ class MainActivity : AppCompatActivity(), AppUpdateView {
      * @see AppUpdateState.userCanceledUpdate
      */
     override fun updateReady() {
+        log("Update ready...")
         Snackbar
             .make(binding.root,"An update has just been downloaded.", Snackbar.LENGTH_INDEFINITE)
             .setAction("Update") { updateWrapper.userConfirmedUpdate() }
@@ -62,6 +99,29 @@ class MainActivity : AppCompatActivity(), AppUpdateView {
      * Critical update error occurred e.g. when immediate update was requested but failed to proceed
      */
     override fun updateFailed(e: Throwable) {
-        Toast.makeText(this, "Update failed", Toast.LENGTH_SHORT).show()
+        log(e, "updateFailed")
+    }
+
+    /**
+     * Notify user of some non-critical update error e.g. flexible update has failed but it is not critical for
+     * general application flow.
+     * Called in flexible flow
+     */
+    override fun nonCriticalUpdateError(e: Throwable) {
+        log(e, "nonCriticalUpdateError")
+    }
+
+    /**
+     * Debug output
+     */
+    private fun log(e: Throwable, message: String) {
+        log("$message: $e")
+    }
+
+    /**
+     * Debug output
+     */
+    private fun log(message: String) {
+        binding.textView.append("\n--> $message")
     }
 }
